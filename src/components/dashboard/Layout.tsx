164ff84e4f617726ebf2ff2b791be83ba3b63cac
WiloxW <wilian.quintero@correounivalle.edu.co>
2025-07-16 21:23:38 -0500

    creacion del front para la ventana Billing

--- src/components/dashboard/Layout.tsx
+++ src/components/dashboard/Layout.tsx
@@ -5,10 +5,12 @@ import { useState } from "react";
 import Header from "../header/Header";
 import Sidebar from "../sidebar/sidebar";
 import Dashboard from "./Dashboard";
+import Purchase from "../Metricspreview/PurchaseMod"
 import Modal from "../Metricspreview/Modal";
 
 export function Layout() {
   const [activeView, setActiveView] = useState("dashboard");
+  const [isPurchaseOpen, setIsPurchaseOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleMenuClick = (menuKey: string) => {
@@ -16,6 +18,8 @@ export function Layout() {
     if (menuKey === "add-item") {
       setIsModalOpen(true);
       console.log("Modal should open now"); // Para depuración
+    } else if (menuKey === "billing") {
+    setIsPurchaseOpen(true);
     } else {
       setActiveView(menuKey);
     }
@@ -27,6 +31,7 @@ export function Layout() {
         return <Dashboard />;
       case "edit-product":
       case "inventory":
+      case "billing":
       case "users":
         return <Dashboard />;
       default:
@@ -45,6 +50,11 @@ export function Layout() {
       <Modal isOpen={isModalOpen} onClose={() => {
         console.log("Closing modal"); // Para depuración
         setIsModalOpen(false);
+      }} />
+
+      <Purchase isOpen={isPurchaseOpen} onClose={() => {
+        console.log("Closing purchase modal");
+        setIsPurchaseOpen(false);
       }} />
     </div>
   );
